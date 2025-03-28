26e7001272e2601d6d709397c14a9c502cb29ce2
nithi <nithiyananatham@erssmail.com>
2025-03-28 20:49:41 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Ganesan J',
+    author_email='ganesanja@gmail.com',
+    packaages=find_packages(),
+    install_requires=[]
+    
+)
